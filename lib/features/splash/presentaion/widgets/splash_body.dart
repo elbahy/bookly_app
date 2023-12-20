c399aad2308d7649ff1815e228dc70d537e45a0a
Mohamed Elbahy <vb.supporter@gmail.com>
2023-12-20 23:42:04 +0200

    improve code

--- lib/features/splash/presentaion/widgets/splash_body.dart
+++ lib/features/splash/presentaion/widgets/splash_body.dart
@@ -3,7 +3,6 @@ import 'package:bookly_app/features/home/presentation/screens/home_screen.dart';
 import 'package:bookly_app/features/splash/presentaion/widgets/slider_animation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
@@ -16,12 +15,22 @@ class _SplashBodyState extends State<SplashBody>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation<Offset> sliderAnimation;
+
   @override
   void initState() {
-    initSliderAnimation();
-    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    sliderAnimation =
+        Tween<Offset>(begin: const Offset(0, 8), end: const Offset(0, 0))
+            .animate(animationController);
 
-    navigateToHomeScreen();
+    animationController.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(const HomeScreen(), transition: Transition.leftToRight);
+    });
+
+    super.initState();
   }
 
   @override
@@ -37,24 +46,16 @@ class _SplashBodyState extends State<SplashBody>
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        slideAnimation(sliderAnimation: sliderAnimation)
+        AnimatedBuilder(
+            animation: sliderAnimation,
+            builder: (context, _) {
+              return SlideTransition(
+                position: sliderAnimation,
+                child:
+                    const Text('Read Free Books', textAlign: TextAlign.center),
+              );
+            })
       ],
     );
-  }
-
-  void initSliderAnimation() {
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-
-    sliderAnimation = Tween<Offset>(begin: const Offset(0, 8), end: Offset.zero)
-        .animate(animationController);
-
-    animationController.forward();
-  }
-
-  void navigateToHomeScreen() {
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.to(const HomeScreen(), transition: Transition.rightToLeft);
-    });
   }
 }
